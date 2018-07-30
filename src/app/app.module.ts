455ad37589913b7fedd022c351d8a931df1a81e8
TXT-17 <txt-17@TXT-MacBook-Pro-17.local>
2018-07-30 10:44:20 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TechCurriculumComponent } from './tech-curriculum/tech-curriculum.component';
+import { ProductDevelopmentComponent } from './product-development/product-development.component';
+import { YouthDevelopmentComponent } from './youth-development/youth-development.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TechCurriculumComponent,
+    ProductDevelopmentComponent,
+    YouthDevelopmentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tech-curriculum/tech-curriculum.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tech-curriculum',
+  templateUrl: './tech-curriculum.component.html',
+  styleUrls: ['./tech-curriculum.component.scss']
+})
+export class TechCurriculumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
